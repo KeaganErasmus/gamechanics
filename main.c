ad38a49fdbcd547af945b488ebafe55d2c196258
keagan erasmus <keaganerasmus12@gmail.com>
2024-07-01 19:55:03 +1200

    can throw hook on x axis

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include "raylib.h"
+#include "raymath.h"
 #include "stdio.h"
+
+bool reached = false;
+bool throw_hook = false;
+float range = 500;
+
+typedef struct Hook
+{
+    Vector2 pos;
+    Vector2 target;
+} Hook;
+
 typedef struct Player
 {
     Vector2 pos;
     Vector2 size;
     Vector2 target;
     bool should_move;
+    float dir;
+    Vector2 hook_loc;
+    Hook hook;
 } Player;
 
 Player InitPlayer() {
@@ -14,12 +29,45 @@ Player InitPlayer() {
     player.size = (Vector2){20, 20};
     player.target = (Vector2){0, 0};
     player.should_move = false;
-
+    player.dir = 0.0;
+    player.hook_loc = (Vector2){0, 0};
+    player.hook.pos = player.pos;
+    player.hook.target = player.hook_loc;
     return player;
 }
 
+void DrawHook(Player *p) {
+    Vector2 start = p->pos;
+    Vector2 end = p->hook_loc;
+
+    float distance = Vector2Distance(start, end); 
+
+    Vector2 target_pos = Vector2Lerp(start, end, GetFrameTime());
+
+    if(throw_hook) {
+        if(p->hook.pos.x < end.x && !reached){
+            p->hook.pos.x += 2;
+        }
+        if(p->hook.pos.x >= end.x){
+            reached = true;
+        }
+        
+        if(reached) {
+            p->hook.pos.x -= 2;
+        }
+
+        if(p->hook.pos.x == p->pos.x){
+            p->hook.pos = p->pos;
+            reached = false;
+            throw_hook = false;
+        }
+
+        DrawRectangleV(p->hook.pos, (Vector2){5, 5}, BLACK);
+    }
+}
+
 void DrawPlayer(Player *p) {
-   DrawRectangleV(p->pos, p->size, RED); 
+    DrawRectangleV(p->pos, p->size, RED); 
 }
 
 void UpdatePlayer(Player *p) {
@@ -28,9 +76,23 @@ void UpdatePlayer(Player *p) {
         Vector2 mouse_pos = GetMousePosition();
         p->target = mouse_pos;
         p->should_move = true;
-        printf("mouse_x: %f\nmouse_y: %f\n", mouse_pos.x, mouse_pos.y);
+
+        float distance = Vector2Distance(p->pos, p->target); 
+        p->dir = Vector2Angle(p->pos, p->target);
+        printf("mouse_x: %f\n", distance);
+
     }
 
+    if(IsKeyPressed(KEY_Q)){
+        Vector2 mouse_pos = GetMousePosition();
+        p->hook_loc = mouse_pos;
+        throw_hook = true;
+    }
+    
+
+    if(throw_hook) {
+        DrawHook(p);
+    }
     // Very basic move to click
     if(p->should_move && p->pos.x < p->target.x) {
         p->pos.x += 5;
