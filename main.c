feee369a7b92e1f9be4f6a34c4fcf13345cbb6ed
keagan erasmus <keaganerasmus12@gmail.com>
2024-07-03 06:55:59 +1200

    hook now goes to target and comes back

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 bool reached = false;
 bool throw_hook = false;
+bool going_back = false;
 float range = 500;
 
 typedef struct Hook
@@ -21,6 +22,7 @@ typedef struct Player
     Vector2 size;
     Vector2 target;
     bool should_move;
+    Rectangle coll;
     float dir;
     Vector2 hook_loc;
     Hook hook;
@@ -32,6 +34,7 @@ Player InitPlayer() {
 
     player.pos = (Vector2){200, 200};
     player.size = (Vector2){20, 20};
+    player.coll = (Rectangle){.height=20, .width=20, .x=player.pos.x, player.pos.y};
     player.target = (Vector2){0, 0};
     player.should_move = false;
     player.dir = 0.0;
@@ -55,19 +58,29 @@ void UpdateHook(Player *p) {
     Vector2 start = p->pos; 
     Vector2 end = p->hook_loc;
     Vector2 direction = Vector2Normalize(Vector2Subtract(start, end));
+    Vector2 dir_to_player = Vector2Normalize(Vector2Subtract(end, start));
     
     // Moves hook towards mouse click
     if(!Vector2Equals(p->hook.pos, p->hook_loc) && !reached){
         p->hook.pos = Vector2Add(p->hook.pos, Vector2Negate(direction));
-
-        p->hook.coll.x = p->hook.pos.x;
-        p->hook.coll.y = p->hook.pos.y;
     }
-    
+
+    // Move hook back towards player 
     if(CheckCollisionRecs(p->hook.coll, p->hook_target)) {
         reached = true;
+        going_back = true;
     }
 
+    if(going_back){
+        p->hook.pos = Vector2Add(p->hook.pos, Vector2Negate(dir_to_player));
+        if(CheckCollisionRecs(p->hook.coll, p->coll)) {
+            throw_hook = false;
+            going_back = false;
+        }
+    }
+    
+    p->hook.coll.x = p->hook.pos.x;
+    p->hook.coll.y = p->hook.pos.y;
     // Debug draw the target loc of hook
     DrawRectangle(p->hook_target.x, p->hook_target.y, p->hook_target.width, p->hook_target.height, BLUE);
 }
@@ -116,6 +129,9 @@ void UpdatePlayer(Player *p) {
     if(p->should_move && p->pos.y > p->target.y) {
         p->pos.y -= 5;
     }
+
+    p->coll.x = p->pos.x;
+    p->coll.y = p->pos.y;
 
     // set abilities to qwer keys
     
