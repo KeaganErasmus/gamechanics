df92e964c558eab5a88b901604fb1b3fdca804d9
keagan erasmus <keaganerasmus12@gmail.com>
2024-07-02 19:57:10 +1200

    hook now goes to where mouse was when clicking q

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include "raymath.h"
 #include "stdio.h"
 
+#include "utils.c"
+
 bool reached = false;
 bool throw_hook = false;
 float range = 500;
 
 typedef struct Hook
 {
+    Rectangle coll;
     Vector2 pos;
-    Vector2 target;
+    float speed;
 } Hook;
 
 typedef struct Player
@@ -21,53 +24,52 @@ typedef struct Player
     float dir;
     Vector2 hook_loc;
     Hook hook;
+    Rectangle hook_target;
 } Player;
 
 Player InitPlayer() {
     Player player = {0};
+
     player.pos = (Vector2){200, 200};
     player.size = (Vector2){20, 20};
     player.target = (Vector2){0, 0};
     player.should_move = false;
     player.dir = 0.0;
-    player.hook_loc = (Vector2){0, 0};
+    player.hook_loc = player.pos; 
     player.hook.pos = player.pos;
-    player.hook.target = player.hook_loc;
+    player.hook.speed = 10;
+    player.hook_target = (Rectangle){.height=5, .width=5, .x=player.pos.x, .y=player.pos.y}; 
+    player.hook.coll = (Rectangle){.height=5, .width=5, .x=player.hook_target.x, player.hook_target.y};
     return player;
 }
 
 void DrawHook(Player *p) {
-    Vector2 start = p->pos;
-    Vector2 end = p->hook_loc;
-
-    float distance = Vector2Distance(start, end); 
-
-    Vector2 target_pos = Vector2Lerp(start, end, GetFrameTime());
-
-    if(throw_hook) {
-        if(p->hook.pos.x < end.x && !reached){
-            p->hook.pos.x += 2;
-        }
-        if(p->hook.pos.x >= end.x){
-            reached = true;
-        }
-        
-        if(reached) {
-            p->hook.pos.x -= 2;
-        }
-
-        if(p->hook.pos.x == p->pos.x){
-            p->hook.pos = p->pos;
-            reached = false;
-            throw_hook = false;
-        }
-
-        DrawRectangleV(p->hook.pos, (Vector2){5, 5}, BLACK);
-    }
+    DrawRectangleV(p->hook.pos, (Vector2){5, 5}, BLACK);
 }
 
 void DrawPlayer(Player *p) {
     DrawRectangleV(p->pos, p->size, RED); 
+}
+
+void UpdateHook(Player *p) {
+    Vector2 start = p->pos; 
+    Vector2 end = p->hook_loc;
+    Vector2 direction = Vector2Normalize(Vector2Subtract(start, end));
+    
+    // Moves hook towards mouse click
+    if(!Vector2Equals(p->hook.pos, p->hook_loc) && !reached){
+        p->hook.pos = Vector2Add(p->hook.pos, Vector2Negate(direction));
+
+        p->hook.coll.x = p->hook.pos.x;
+        p->hook.coll.y = p->hook.pos.y;
+    }
+    
+    if(CheckCollisionRecs(p->hook.coll, p->hook_target)) {
+        reached = true;
+    }
+
+    // Debug draw the target loc of hook
+    DrawRectangle(p->hook_target.x, p->hook_target.y, p->hook_target.width, p->hook_target.height, BLUE);
 }
 
 void UpdatePlayer(Player *p) {
@@ -79,20 +81,28 @@ void UpdatePlayer(Player *p) {
 
         float distance = Vector2Distance(p->pos, p->target); 
         p->dir = Vector2Angle(p->pos, p->target);
-        printf("mouse_x: %f\n", distance);
 
     }
-
+    
     if(IsKeyPressed(KEY_Q)){
         Vector2 mouse_pos = GetMousePosition();
         p->hook_loc = mouse_pos;
+        p->hook.pos = p->pos;
+        p->hook_target.x = p->hook_loc.x;
+        p->hook_target.y = p->hook_loc.y;
+
+        // spawn hook collider
+        p->hook.coll.x = p->hook.pos.x;
+        p->hook.coll.y = p->hook.pos.y;
+
         throw_hook = true;
     }
-    
 
     if(throw_hook) {
+        UpdateHook(p);
         DrawHook(p);
     }
+
     // Very basic move to click
     if(p->should_move && p->pos.x < p->target.x) {
         p->pos.x += 5;
@@ -108,6 +118,7 @@ void UpdatePlayer(Player *p) {
     }
 
     // set abilities to qwer keys
+    
 }
 
 int main(){
